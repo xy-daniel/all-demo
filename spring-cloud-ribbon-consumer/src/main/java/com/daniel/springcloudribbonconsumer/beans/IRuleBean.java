29d482b0aaf3e7d0c4d0a954ace9b0108355d33f
daniel <1207350458@qq.com>
2022-10-14 01:17:43 +0800

    修改ribbon负载均衡规则

--- spring-cloud-ribbon-consumer/src/main/java/com/daniel/springcloudribbonconsumer/beans/IRuleBean.java
+++ spring-cloud-ribbon-consumer/src/main/java/com/daniel/springcloudribbonconsumer/beans/IRuleBean.java
@@ -2,6 +2,7 @@ package com.daniel.springcloudribbonconsumer.beans;
 
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.WeightedResponseTimeRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,7 @@ public class IRuleBean {
 
     @Bean
     public IRule ribbonRule() {
-        return new RandomRule();
+//        return new RandomRule();
+        return new WeightedResponseTimeRule(); //随机数+总响应时长-平均响应时间
     }
 }
